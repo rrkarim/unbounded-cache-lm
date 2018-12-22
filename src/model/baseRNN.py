bc98009e30df6407222027dcf0b31727c8f162f6
coderinuse <yoatom96@hotmail.com>
2018-12-22 22:31:14 +0300

    refactoring

--- src/model/baseRNN.py
+++ src/model/baseRNN.py
@@ -1,8 +1,18 @@
 """ Base class for RNN"""
 import torch.nn as nn
 
+
 class BaseRNN(nn.Module):
-    def __init__(self, vocab_size, max_len, hidden_size, input_dropout_p, dropout_p, n_layers, rnn_cell):
+    def __init__(
+        self,
+        vocab_size,
+        max_len,
+        hidden_size,
+        input_dropout_p,
+        dropout_p,
+        n_layers,
+        rnn_cell,
+    ):
         super(BaseRNN, self).__init__()
         self.vocab_size = vocab_size
         self.max_len = max_len
@@ -10,9 +20,9 @@ class BaseRNN(nn.Module):
         self.n_layers = n_layers
         self.input_dropout_p = input_dropout_p
         self.input_dropout = nn.Dropout(p=input_dropout_p)
-        if rnn_cell.lower() == 'lstm':
+        if rnn_cell.lower() == "lstm":
             self.rnn_cell = nn.LSTM
-        elif rnn_cell.lower() == 'gru':
+        elif rnn_cell.lower() == "gru":
             self.rnn_cell = nn.GRU
         else:
             raise NotImplementedError("Unsupported RNN Cell: {0}".format(rnn_cell))
