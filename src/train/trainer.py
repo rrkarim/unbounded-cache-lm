3167ae9b24957e20fe82bb16914684b0b89e272e
coderinuse <yoatom96@hotmail.com>
2019-01-07 02:27:24 +0300

    add trainer

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -14,8 +14,17 @@ from cachemodel.loss import NLLLoss
 from cachemodel.optim import Optimizer
 from cachemodel.util.checkpoint import Checkpoint
 
+
 class SupervisedTrainer(object):
-    def __init__(self, expt_dir='experiment', loss = NLLLoss(), batch_size=64, random+seed=None, checkpoint_every=500, print_every=1000):
+    def __init__(
+        self,
+        expt_dir="experiment",
+        loss=NLLLoss(),
+        batch_size=64,
+        random_seed=None,
+        checkpoint_every=500,
+        print_every=1000,
+    ):
         self._trainer = "Trainer"
         self.random_seed = random_seed
         if random_seed is not None:
@@ -36,19 +45,44 @@ class SupervisedTrainer(object):
 
         self.logger = logging.getLogger(__name__)
 
-    def _train_batch(self, input_variable, input_lengths, target_variable, model, teacher_forcing_ratio):
+    def _train_batch(
+        self,
+        input_variable,
+        input_lengths,
+        target_variable,
+        model,
+        teacher_forcing_ratio,
+    ):
         loss = self.loss
-        decoder_outputs, decoder_hidden, other = model(input_variable, input_lengths, target_variable, teacher_forcing_ratio=teacher_forcing_ratio)
+        decoder_outputs, decoder_hidden, other = model(
+            input_variable,
+            input_lengths,
+            target_variable,
+            teacher_forcing_ratio=teacher_forcing_ratio,
+        )
         loss.reset()
         for step, step_output in enumerate(decoder_outputs):
             batch_size = target_variable.size(0)
-            loss.eval_batch(step_output.contiguous().view(batch_size, -1), target_variable[:, step+1])
+            loss.eval_batch(
+                step_output.contiguous().view(batch_size, -1),
+                target_variable[:, step + 1],
+            )
 
         model.zero_grad()
         loss.backward()
         self.optimizer.step()
         return loss.get_loss()
-    def _train_epoches(self, data, model, n_epochs, start_epoch, start_step, dev_data=None, teacher_forcing_ratio=0):
+
+    def _train_epoches(
+        self,
+        data,
+        model,
+        n_epochs,
+        start_epoch,
+        start_step,
+        dev_data=None,
+        teacher_forcing_ratio=0,
+    ):
         log = self.logger
 
         print_loss_total = 0
@@ -56,11 +90,14 @@ class SupervisedTrainer(object):
 
         device = None if torch.cuda.is_available() else -1
         batch_iterator = torchtext.data.BucketIterator(
-                dataset=data, batch_size=self.batch_size,
-                sort=False, sort_withing_batch=True,
-                sort_key=lambda x: len(x.src),
-                device=device, repeat=False
-                )
+            dataset=data,
+            batch_size=self.batch_size,
+            sort=False,
+            sort_withing_batch=True,
+            sort_key=lambda x: len(x.src),
+            device=device,
+            repeat=False,
+        )
 
         steps_per_epoch = len(batch_iterator)
         total_steps = steps_per_epoch * n_epochs
@@ -72,5 +109,3 @@ class SupervisedTrainer(object):
             log.debug("Epoch: %d, Step: %d" % (epoch, step))
 
             batch_generator = batch_iterator.__iter__()
-
-
