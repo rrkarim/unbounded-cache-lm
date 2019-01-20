2bf6e4f4b809c1474b596783280f462969e12cbe
Rasul Karimov <yoatom96@hotmail.com>
2019-01-21 01:24:46 +0300

    sample

--- cachemodel/model/cache.py
+++ cachemodel/model/cache.py
@@ -6,7 +6,7 @@ import faiss
 
 
 class BaseCache(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab, alpha):
         super(BaseCache, self).__init__()
         self.vectors = [] # high memory usage here
         self.vocab = vocab
@@ -21,11 +21,11 @@ class BaseCache(nn.Module):
 
 class Cache(BaseCache):
     def __init__(self, vocab=None, alpha=0.5, smooth=0.2):
-        super(Cache, self).__init__()
+        super(Cache, self).__init__(vocab, alpha)
         self.smooth = smooth
 
     def _add_element(self, item, hidden):
-        self.cache_elems.append((item, hidden))
+        self.cache_elems.append((item, torch.squeeze(hidden)))
         self.vectors.append(hidden)
         self.word_to_index[item] = len(self.cache_elems) - 1
 
@@ -39,20 +39,20 @@ class Cache(BaseCache):
     def calculate_sum(self, h_t):
         cache_p = torch.zeros([1, len(self.vocab.itos)])
         for i, v in enumerate(self.vocab.itos):
-            cache_p[0][i] = self.get_sum(h_t[i], v)
+            cache_p[0][i] = self.get_sum(h_t, v)
         cache_p = F.log_softmax(cache_p)
         return cache_p
 
 
-class CacheKmeans(BaseCache):
+class CacheKMeans(BaseCache):
 
-    def __init__(self, kn):
-        super(CacheKmeans, self).__init__()
+    def __init__(self, vocab=None, alpha=0.5, smooth=0.2, kn=10):
+        super(CacheKMeans, self).__init__(vocab=vocab, alpha=alpha)
         self.kn = kn
 
     def get_sum(self, h_t, v):
         sum_ = 0.
-        list_indeces, kth_neighb = self._find_neighbors(k=3, h_t)
+        list_indeces, kth_neighb = self._find_neighbors(k=3, h_t=h_t)
         for index in list_indeces:
             w_i, h_i = self.cache_elems[index]
             if w_i == v:
@@ -67,6 +67,7 @@ class CacheKmeans(BaseCache):
         index = faiss.IndexIVFPQ(quantizer, d, nlist, m, 8)
         index.train(self.vectors)
         index.add(self.vectors)
+
         D,I = index.search([h_t], k)
         return I, D[-1][-1]
 
